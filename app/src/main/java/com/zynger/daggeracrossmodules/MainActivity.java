5b563a5db9122ae0c49edf197fc345ed3d5078ad
Julio Zynger <julio.zynger@soundcloud.com>
2017-10-11 10:15:58 +0200

    Add dependency of collab module into app module

--- app/src/main/java/com/zynger/daggeracrossmodules/MainActivity.java
+++ app/src/main/java/com/zynger/daggeracrossmodules/MainActivity.java
@@ -3,11 +3,15 @@ package com.zynger.daggeracrossmodules;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zynger.collaborators.Collaborator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Collaborator collaborator = new Collaborator();
     }
 }
