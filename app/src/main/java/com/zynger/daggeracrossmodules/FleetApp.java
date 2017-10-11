80da8ed7dcfb2e603e6dacaddae21627ed4df982
Julio Zynger <julio.zynger@soundcloud.com>
2017-10-11 10:46:31 +0200

    Generate application component by instantiating app module

--- app/src/main/java/com/zynger/daggeracrossmodules/FleetApp.java
+++ app/src/main/java/com/zynger/daggeracrossmodules/FleetApp.java
@@ -1,6 +1,25 @@
 package com.zynger.daggeracrossmodules;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.zynger.daggeracrossmodules.di.ApplicationComponent;
+import com.zynger.daggeracrossmodules.di.ApplicationModule;
+import com.zynger.daggeracrossmodules.di.DaggerApplicationComponent;
 
 public class FleetApp extends Application {
+
+    private ApplicationComponent applicationComponent;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        this.applicationComponent = buildApplicationComponent();
+    }
+
+    private ApplicationComponent buildApplicationComponent() {
+        return DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
 }
